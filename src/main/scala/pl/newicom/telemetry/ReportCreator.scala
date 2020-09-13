e998b9d3d3dccedd9f6e6d73034ebe18bcfe9149
pawelkaczor <newion@o2.pl>
2020-09-13 09:59:47 +0200

    minor

--- src/main/scala/pl/newicom/telemetry/ReportCreator.scala
+++ src/main/scala/pl/newicom/telemetry/ReportCreator.scala
@@ -17,7 +17,7 @@ object ReportCreator {
     Future
       .sequence {
         dailyReportSources(directory)
-          .map(_.fold(repBuilder)((b, m) => { b.withMeasurement(m) }))
+          .map(_.fold(repBuilder)(_.withMeasurement(_)))
           .map(_.toMat(Sink.seq)(Keep.right))
           .map(_.run())
       }
